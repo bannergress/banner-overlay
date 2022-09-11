bfb06331a5e50af4721570550543a715516e54f2
Eckhart Wörner <ewoerner@users.noreply.github.com>
2022-09-11 17:07:09 +0200

    Fix first start (permission) flow.

--- app/src/main/java/com/bannergress/overlay/MainActivity.java
+++ app/src/main/java/com/bannergress/overlay/MainActivity.java
@@ -1,6 +1,7 @@
 package com.bannergress.overlay;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
 
@@ -9,21 +10,10 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private ActivityResultLauncher<Intent> requestPermissionResultLauncher;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        requestPermissionResultLauncher = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                result -> startService(false)
-        );
-
-        startService(true);
-
-        finish();
-    }
+    private final ActivityResultLauncher<Intent> requestPermissionResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> startService(false)
+    );
 
     private void startService(boolean requestPermission) {
         if (Settings.canDrawOverlays(this)) {
@@ -31,9 +21,20 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, OverlayService.class);
             intent.putExtra(Intent.EXTRA_TEXT, data);
             startService(intent);
+            finish();
         } else if (requestPermission) {
-            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
             requestPermissionResultLauncher.launch(intent);
+        } else {
+            finish();
         }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        startService(true);
     }
 }
