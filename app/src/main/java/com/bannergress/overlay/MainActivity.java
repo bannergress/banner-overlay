f2e299ccc644824d1e7e155cfae73a32ca1bfd68
xscreach <xscreach@users.noreply.github.com>
2023-07-09 07:24:42 +0200

    organised imports

--- app/src/main/java/com/bannergress/overlay/MainActivity.java
+++ app/src/main/java/com/bannergress/overlay/MainActivity.java
@@ -2,7 +2,12 @@ package com.bannergress.overlay;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.webkit.*;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
